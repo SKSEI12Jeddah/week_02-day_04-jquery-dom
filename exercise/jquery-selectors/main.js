113164b905001acb094cbc87107b883b98c15e45
saadkhan29 <57666391+saadkhan29@users.noreply.github.com>
2020-03-02 21:48:46 +0300

    Update main.js

--- exercise/jquery-selectors/main.js
+++ exercise/jquery-selectors/main.js
@@ -1,11 +1,11 @@
 // 1.  Select the H1
 let heading = ????
-console.log(heading)
+//alert(heading)
 
 // 2.  Select all of the list items
 let listItems = ????
-console.log(listItems)
+//alert(listItems)
 
 // 3.  Select only the last list item
 let listItem = ????
-console.log(listItem)
+//alert(listItem)
